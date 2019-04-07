3f4536e86b5c0e88fbc9239d45b9369b4d44f754
John <kruse67@live.com>
2019-04-07 10:34:34 -0600

    Updates

---
+++ precache-manifest.964966e2129fda85e1ea7753e10d13a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clicky-game/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "70cb77b58e7dd12ce5ea",
+    "url": "/clicky-game/static/js/main.3646ddb9.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clicky-game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "70cb77b58e7dd12ce5ea",
+    "url": "/clicky-game/static/css/main.0097a5c6.chunk.css"
+  },
+  {
+    "revision": "9764e79fdee9af607858cf9b7e1e1119",
+    "url": "/clicky-game/index.html"
+  }
+];
