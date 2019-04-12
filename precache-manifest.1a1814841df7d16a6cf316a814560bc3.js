5857e7ef7dcb728786262fb07f96548cd1f4c07a
John <kruse67@live.com>
2019-04-11 21:42:24 -0600

    Updates

---
+++ precache-manifest.1a1814841df7d16a6cf316a814560bc3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "40c4c39cd622122a2a4e",
+    "url": "/clicky-game/static/js/main.4d53f44f.chunk.js"
+  },
+  {
+    "revision": "2e75b5943902ac95e22d",
+    "url": "/clicky-game/static/js/2.c999b0f2.chunk.js"
+  },
+  {
+    "revision": "9265b28449e651378e7b0ba51d78e225",
+    "url": "/clicky-game/index.html"
+  }
+];
